8087ed8fc63e4ddd5746595fce633a667323f603
Ahmet_Emin <ahmeteminsaglik@gmail.com>
2023-01-04 17:36:01 +0300

    broken files are deleted

--- src/main/java/org/aes/Main.java
+++ src/main/java/org/aes/Main.java
@@ -1,5 +1,9 @@
 package org.aes;
 
+import org.aes.model.FileLocation;
+import org.ahmeteminsaglik.fileoperation.entities.concretes.FileFundamental;
+
+import java.util.List;
 import java.util.logging.Logger;
 
 public class Main {
@@ -13,8 +17,28 @@ public class Main {
          * deleted words that involves joker characters
          * if word has alphabetic chracters than get it.
          * */
-        FileOperation fileOperation = new FileOperation();
-        fileOperation.createUniqeWords();
+
+        /*
+        * File operationda file okuma, file location bilgileri olmali
+        * uniqe word olusturma ile ilgili baska bir class olmali ve bu class'taki fonksiyon cagrilirken
+          her seferinde fileFund gonderilmeli
+        * islem bitince getUniqeWord ile liste seklinde alinmali
+        * Sonra Word objesine cevrilmeli
+        * Sonra db'ye kaydeidlmeli
+        *
+        * */
+//        FileOperation fileOperation = new FileOperation();
+//        List<FileFundamental> fileFundList = fileOperation.getBookFileFundementalList();
+//        WordDataOperation wordDataOperation= new WordDataOperation();
+//        wordDataOperation.createUniqeWords(fileFundList);
+
+        //40_881, before clear broken books
+        //38_709,  after deleted broken books.
+        FileLocation fileLocation = new FileLocation();
+        List<FileFundamental> fileFundList = fileLocation.getBookFileFundementalList();
+
+        WordDataOperation wordDataOperation = new WordDataOperation();
+        wordDataOperation.createUniqeWords(fileFundList);
     }
 
 }
